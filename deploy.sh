7f89d9980ac71e3ef5a4ea33c77a37105fa060c8
Lauramuke <lauramuke12@gmail.com>
2024-05-23 12:27:49 -0400

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 Application is ready to be deployed to the development environment
-development must be reviewed by manager before deployment.
+development must be reviewed by manager before deployment to the environment.
